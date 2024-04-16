c4e99d677574855bb5dcd804710848fde1cd759d
malzbanker <154757574+malzbanker@users.noreply.github.com>
2024-04-16 14:46:48 +0300

    baseURL added

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const axiosconfig=axios.create({
-    baseURL:"http://localhost:5500/api"
+    // baseURL: "http://localhost:5500/api"
+    baseURL:"https://forum-backend-hwau.onrender.com/api"
 })
 export default axiosconfig
--- src/configaxios.js
+++ src/configaxios.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 const axiosconfigbase= axios.create({
-    baseURL:"http://localhost:5500/api"
+    // baseURL: "http://localhost:5500/api"
+    baseURL:"https://forum-backend-hwau.onrender.com/api"
+    
 })
 export default axiosconfigbase
